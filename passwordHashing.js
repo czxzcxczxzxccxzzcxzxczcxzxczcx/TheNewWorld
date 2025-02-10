00d28434e2f0c42e4b797929e5407315b2cc2cda
czxzcxczxzxccxzzcxzxczcxzxczcx <76052291+czxzcxczxzxccxzzcxzxczcxzxczcx@users.noreply.github.com>
2025-02-10 13:24:56 -0500

    Create passwordHashing.js

---
+++ passwordHashing.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const hashPassword = async (password) => 
+{
+    return await bcrypt.hash(password, saltRounds);
+};
+
+const comparePassword = async (inputPassword, storedPasswordHash) => 
+{
+    return await bcrypt.compare(inputPassword, storedPasswordHash);
+};
+
+module.exports = { hashPassword, comparePassword };
